823abcd7bd4c02c9077e36e6c42b5bbfb3d9adda
JamsNJellies <jamespalmer674@gmail.com>
2018-10-02 18:40:07 +0100

    Prevent downlaading of many thyme binaries

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # Usage install script - JamsNJellies
 
-go get -u github.com/sourcegraph/thyme/cmd/thyme
+echo "[INFO] Usage install script by JamsNJellies"
+echo "[INFO] Beggining install"
+echo "[INFO] Getting thyme binary"
+if [[ -f thyme-linux-386 ]]; then
+	rm thyme-linux-386
+fi
+
+wget https://github.com/sourcegraph/thyme/releases/download/0.2.3/thyme-linux-386 &> /dev/null
+
+echo "[INFO] Moving files to required locations"
 sudo cp usage /bin/
+sudo cp thyme-linux-386 /bin/thyme
+sudo chmod +x /bin/thyme
+echo "[INFO] Installing desktop files"
 sudo cp usage.desktop /usr/share/applications/
+
+echo "Thyme dependencies - REQUIRED!"
+echo ""
+thyme dep
